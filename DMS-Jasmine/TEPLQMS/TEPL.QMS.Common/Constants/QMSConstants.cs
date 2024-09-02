86fc08c5b9a61f198453b60b62b5a37a6a07da69
Madhusudhanav <madhu.vangala@canopusgbs.com>
2024-09-02 18:53:19 +0530

    Function and Location changes added - Maria

--- DMS-Jasmine/TEPLQMS/TEPL.QMS.Common/Constants/QMSConstants.cs
+++ DMS-Jasmine/TEPLQMS/TEPL.QMS.Common/Constants/QMSConstants.cs
@@ -139,5 +139,13 @@ namespace TEPL.QMS.Common.Constants
         public static string spGetUserApprovalItemsForAMID = "spGetUserApprovalItemsForAMID";
         public static string spValidateDocumentNumber = "spValidateDocumentNumber";
         public static string spCreatePrintRequest = "spCreatePrintRequest";
+        public static string spAddFunction = "spAddFunction";
+        public static string spUpdateFunction = "spUpdateFunction";
+        public static string spDeleteFunction = "spDeleteFunction"; 
+        public static string spGetFunctions = "spGetFunctions";
+        public static string spAddLocation = "spAddLocation";
+        public static string spUpdateLocation = "spUpdateLocation";
+        public static string spDeleteLocation = "spDeleteLocation";
+        public static string spGetLocation = "spGetLocation";
     }
 }
